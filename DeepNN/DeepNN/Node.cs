a74e4d3f8c7b767eca3fa098631131ae55a5b68c
vytautas-dotcom <tumas.vytautas@gmail.com>
2022-04-14 14:29:32 +0300

    Create Node

---
+++ DeepNN/DeepNN/Node.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeepNN
+{
+    public class Node
+    {
+        public List<Connection> InConnections { get; set; }
+        public List<Connection> OutConnections { get; set; }
+        public double Bias { get; set; }
+        public double DeltaBias { get; set; }
+        public double Gradient { get; set; }
+        public double Value { get; set; }
+        public Node()
+        {
+            InConnections = new List<Connection>();
+            OutConnection = new List<Connection>();
+            Bias = Network.GetRandomBias();
+        }
+        public Node(IEnumerable<Node> inNodes) : this()
+        {
+            foreach (Node node in inNodes)
+            {
+                Connection connection = new Connection(node, this);
+                node.OutConnections.Add(connection);
+                InConnections.Add(connection);
+            }
+        }
+    }
+}
